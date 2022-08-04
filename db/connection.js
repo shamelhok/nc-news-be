33c0768f4b8546f2f41693cce9ec8fc0dbfd1801
shamelhok <msh_shamel@hotmail.co.uk>
2022-08-04 15:43:00 +0100

    message

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,8 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
 const config =
@@ -18,5 +18,5 @@ const config =
         },
       }
     : {};
-
+// updated connection
 module.exports = new Pool(config);
