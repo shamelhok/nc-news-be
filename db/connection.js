b2ff9ac6f9f69efd991a08d3a477a5a8311ee812
shamel hok <msh_shamel@hotmail.co.uk>
2022-09-21 14:46:15 +0100

    revert changes to connection,js

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL&&!['development','test'].includes(ENV)) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
