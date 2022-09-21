aeff0056fab868e99346aba5e01dda39199d6848
shamel hok <msh_shamel@hotmail.co.uk>
2022-09-21 14:35:57 +0100

    change connection to not trow error in tests

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL&&!['development','test'].includes(ENV)) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
